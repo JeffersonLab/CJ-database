1eeef62db72440fb2379374afb97e59706c4c9c6
Christopher Cocuzza <ccocuzza@sciml2101.jlab.org>
2025-01-03 16:17:53 -0500

    update

--- data/to_csv.py
+++ data/to_csv.py
@@ -7,10 +7,10 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    idxs = [10037, 10038, 10041, 10042, 10050, 10051, 10052, 10053, 10054, 10055, 10056]
+    idxs = [10003, 10021, 10033, 10037, 10038, 10041, 10042, 10050, 10051, 10052, 10053, 10054, 10055, 10056, 10077, 10086]
 
     for idx in idxs:
-        excel = pd.DataFrame(pd.read_excel('dataframe/%s.xlsx'   %idx))
+        excel = pd.DataFrame(pd.read_excel('dataframe/%s.xlsx'%idx,sheet_name='format'))
     
         excel.to_csv  ('dataframe/csv/%s.csv'%idx)
 
